c9a57c64f7c162e5b9c138a03257a3ffafae864f
QIU023 <yiqiaoqiu@hotmail.com>
2026-09-12 18:23:50 +0000

    integration: the typing import sits with the other imports

--- torchtitan/models/kimi_k3/parallelize.py
+++ torchtitan/models/kimi_k3/parallelize.py
@@ -1,10 +1,10 @@
-from typing import Any
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # All rights reserved.
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+from typing import Any
 import torch.nn as nn
 from torch.distributed.pipelining.schedules import (
     _PipelineSchedule,
